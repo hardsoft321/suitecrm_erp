b85a084a6fdbf3d82ec33fb194969d08ebe3d681
Leon Nikitin <nlv@lab321.ru>
2023-04-27 17:51:03 +0600

    ERP: Админка генерации тестовых квот перенесена в общий раздел ERP

---
+++ custom/Extension/modules/Administration/Ext/Administration/erp_genData_group_admin.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @license http://hardsoft321.org/license/ GPLv3
+ * @author  Denis Antonenko <ads@lab321.ru>
+ * @author  Leon Nikitin <nlv@lab321.ru>
+ */
+// $admin_option_defs = [];
+// $admin_option_defs['Administration']['aos_erp_gen_quotes'] = array(
+//     'AOS_ERP_GEN_QUOTES',
+//     'LBL_AOS_ERP_GEN_QUOTES_TITLE',
+//     'LBL_AOS_ERP_GEN_QUOTES_INFO',
+//     './index.php?module=GenerationERPData&action=GenView',
+//     'diagnostic'
+// );
+
+// $admin_group_header['aos_erp'] = array(
+//     'LBL_OTHER_HEADER',
+//     '',
+//     false,
+//     $admin_option_defs,
+//     ''
+// );
+
+$admin_group_header['aos_erp'][3]['Administration']['aos_erp_gen_quotes'] = array(
+    'AOS_ERP_GEN_QUOTES',
+    'LBL_AOS_ERP_GEN_QUOTES_TITLE',
+    'LBL_AOS_ERP_GEN_QUOTES_INFO',
+    './index.php?module=GenerationERPData&action=GenView',
+    'diagnostic'
+);
+
+
+
--- custom/Extension/modules/Administration/Ext/Language/en_us.hs321_erp_genData.php
+++ custom/Extension/modules/Administration/Ext/Language/en_us.hs321_erp_genData.php
@@ -2,8 +2,9 @@
 /**
  * @license http://hardsoft321.org/license/ GPLv3
  * @author  Denis Antonenko <ads@lab321.ru>
+ * @author  Leon Nikitin <nlv@lab321.ru>
  */
 
 $mod_strings['LBL_OTHER_HEADER'] = 'Other';
-$mod_strings['LBL_GEN_ERP_TITLE'] = 'Generation ERP data';
-$mod_strings['LBL_GEN_ERP_TITLE_INFO'] = 'Generation ERP data';
+$mod_strings['LBL_AOS_ERP_GEN_QUOTES_TITLE'] = 'Generation ERP data';
+$mod_strings['LBL_AOS_ERP_GEN_QUOTES_INFO'] = 'Generation ERP data';
