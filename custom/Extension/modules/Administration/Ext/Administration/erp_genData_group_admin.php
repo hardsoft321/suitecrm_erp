4667e93c7d38ea3584f044b5899d93e3e813130b
Leon Nikitin <nlv@lab321.ru>
2023-04-27 18:29:37 +0600

    Исправлено подключение ERP в админку - порядок пакетов не важен

--- custom/Extension/modules/Administration/Ext/Administration/erp_genData_group_admin.php
+++ custom/Extension/modules/Administration/Ext/Administration/erp_genData_group_admin.php
@@ -4,30 +4,34 @@
  * @author  Denis Antonenko <ads@lab321.ru>
  * @author  Leon Nikitin <nlv@lab321.ru>
  */
-// $admin_option_defs = [];
-// $admin_option_defs['Administration']['aos_erp_gen_quotes'] = array(
-//     'AOS_ERP_GEN_QUOTES',
-//     'LBL_AOS_ERP_GEN_QUOTES_TITLE',
-//     'LBL_AOS_ERP_GEN_QUOTES_INFO',
-//     './index.php?module=GenerationERPData&action=GenView',
-//     'diagnostic'
-// );
 
-// $admin_group_header['aos_erp'] = array(
-//     'LBL_OTHER_HEADER',
-//     '',
-//     false,
-//     $admin_option_defs,
-//     ''
-// );
+ if (isset($admin_group_header['aos_erp'])) {
+    $admin_group_header['aos_erp'][3]['Administration']['aos_erp_gen_quotes'] = array(
+        'AOS_ERP_GEN_QUOTES',
+        'LBL_AOS_ERP_GEN_QUOTES_TITLE',
+        'LBL_AOS_ERP_GEN_QUOTES_INFO',
+        './index.php?module=GenerationERPData&action=GenView',
+        'diagnostic'
+    );
+ } else {
 
-$admin_group_header['aos_erp'][3]['Administration']['aos_erp_gen_quotes'] = array(
-    'AOS_ERP_GEN_QUOTES',
-    'LBL_AOS_ERP_GEN_QUOTES_TITLE',
-    'LBL_AOS_ERP_GEN_QUOTES_INFO',
-    './index.php?module=GenerationERPData&action=GenView',
-    'diagnostic'
-);
+    $admin_option_defs = [];
 
+    $admin_option_defs['Administration']['aos_erp_gen_quotes'] = array(
+        'AOS_ERP_GEN_QUOTES',
+        'LBL_AOS_ERP_GEN_QUOTES_TITLE',
+        'LBL_AOS_ERP_GEN_QUOTES_INFO',
+        './index.php?module=GenerationERPData&action=GenView',
+        'diagnostic'
+    );
+
+    $admin_group_header['aos_erp'] = array(
+        'LBL_AOS_ERP_HEADER',
+        'LBL_AOS_ERP_HEADER',
+        false,
+        $admin_option_defs,
+        ''
+    );
+}
 
 
--- custom/Extension/modules/Administration/Ext/Administration/erp_group_admin.php
+++ custom/Extension/modules/Administration/Ext/Administration/erp_group_admin.php
@@ -1,30 +1,34 @@
 <?php
 /**
  * @license http://hardsoft321.org/license/ GPLv3
- * @author  Denis Antonenko <ads@lab321.ru>
+ * @author  Leon Nikitin <nlv@lab321.ru>
  */
-$admin_option_defs = [];
 
-// $admin_option_defs['Administration']['aos_erp_settings'] = array(
-//     'AOS_ERP_SETTINGS',
-//     'LBL_AOS_ERP_SETTINGS_TITLE',
-//     'LBL_AOS_ERP_SETTINGS_INFO',
-//     'index.php?module=AOS_ERP&action=createPurchaseContracts&return_module=Administration',
-//     'diagnostic'
-// );
+ if (isset($admin_group_header['aos_erp'])) {
+    $admin_group_header['aos_erp'][3]['Administration']['aos_erp_create_purchase_contracts'] = array(
+        'AOS_ERP_CREATE_PURCHASE_CONTRACTS',
+        'LBL_AOS_ERP_CREATE_PURCHASE_CONTRACTS_TITLE',
+        'LBL_AOS_ERP_CREATE_PURCHASE_CONTRACTS_INFO',
+        'index.php?module=AOS_ERP&action=createPurchaseContracts&return_module=Administration',
+        'diagnostic'
+    );        
+ } else {
 
-$admin_option_defs['Administration']['aos_erp_create_purchase_contracts'] = array(
-    'AOS_ERP_CREATE_PURCHASE_CONTRACTS',
-    'LBL_AOS_ERP_CREATE_PURCHASE_CONTRACTS_TITLE',
-    'LBL_AOS_ERP_CREATE_PURCHASE_CONTRACTS_INFO',
-    'index.php?module=AOS_ERP&action=createPurchaseContracts&return_module=Administration',
-    'diagnostic'
-);
+    $admin_option_defs = [];
 
-$admin_group_header['aos_erp'] = array(
-    'LBL_AOS_ERP_HEADER',
-    'LBL_AOS_ERP_HEADER',
-    false,
-    $admin_option_defs,
-    ''
-);
+    $admin_option_defs['Administration']['aos_erp_create_purchase_contracts'] = array(
+        'AOS_ERP_CREATE_PURCHASE_CONTRACTS',
+        'LBL_AOS_ERP_CREATE_PURCHASE_CONTRACTS_TITLE',
+        'LBL_AOS_ERP_CREATE_PURCHASE_CONTRACTS_INFO',
+        'index.php?module=AOS_ERP&action=createPurchaseContracts&return_module=Administration',
+        'diagnostic'
+    );
+
+    $admin_group_header['aos_erp'] = array(
+        'LBL_AOS_ERP_HEADER',
+        'LBL_AOS_ERP_HEADER',
+        false,
+        $admin_option_defs,
+        ''
+    );
+}
